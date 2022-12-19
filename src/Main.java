80c3acf08147c2e8a808989c9574355f591ae51f
kaVII-x <ka52jiguli@yandex.ru>
2022-12-19 19:23:00 +0200

    Первое задание

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+            System.out.println(cat);
+                System.out.println(paper);
+
+            dog = dog + 4;
+            cat = cat + 4;
+            paper = paper + 4;
+            System.out.println(dog);
+                System.out.println(cat);
+                    System.out.println(paper);
+
+                        dog = dog - 3.5;
+                        cat = cat - 1.6;
+                        paper = paper - 7639;
+                        System.out.println(dog);
+                            System.out.println(cat);
+                                System.out.println(paper);
+
+    }
+}
